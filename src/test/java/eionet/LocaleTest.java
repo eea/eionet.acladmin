68f49e499adaf405ea67005cc3ab7def18680fd6
voolajuh <voolajuh@26ecc896-f4e2-0310-9f2f-ca50803180eb>
2011-12-20 14:29:49 +0000

    Refactoring to maven structure.

--- src/test/java/eionet/LocaleTest.java
+++ src/test/java/eionet/LocaleTest.java
@@ -3,16 +3,16 @@ package eionet;
 import junit.framework.TestCase;
 
 /**
- * 
+ *
  * @author Søren Roug, e-mail: <a href="mailto:soren.roug@eea.europa.eu">soren.roug@eea.europa.eu</a>
  *
  */
 public class LocaleTest extends TestCase {
-    
+
 
     public void test_javac_utf8() {
         // Verify that javac can handle "Elektra" in the Greek alphabet
-        char elektra[] = { 
+        char elektra[] = {
             '\u0397', '\u03bb', '\u03ad', '\u03ba', '\u03c4', '\u03c1', '\u03b1'
         };
         String Elektra = new String(elektra);
